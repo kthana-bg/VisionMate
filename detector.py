94fe158de0b16354cbb9c6d85cedd60280417c4c
kthana-bg <kthana7503@gmail.com>
2026-04-15 18:39:38 +0800

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,18 +1,18 @@
 import cv2
 import numpy as np
-import mediapipe as mp
-
-import mediapipe.python.solutions.face_mesh as mp_face_mesh 
 
 class EyeStrainDetector:
+
     def __init__(self):
-        self.face_mesh = mp_face_mesh.FaceMesh(
+        import mediapipe as mp
+        self.mp_face_mesh = mp.solutions.face_mesh
+
+        self.face_mesh = self.mp_face_mesh.FaceMesh(
             max_num_faces=1,
             refine_landmarks=True,
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5
         )
-        # Your eye indices...
         
         self.LEFT_EYE = [362, 385, 387, 263, 373, 380]
         self.RIGHT_EYE = [33, 160, 158, 133, 153, 144]
