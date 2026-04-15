35b81f76ace1bc322310522a3fc2f604cfd09b1d
kthana-bg <kthana7503@gmail.com>
2026-04-15 18:21:21 +0800

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,8 +1,7 @@
 import cv2
 import numpy as np
 import mediapipe as mp
-
-import mediapipe.python.solutions.face_mesh as mp_face_mesh
+mp_face_mesh = mp.solutions.face_mesh
 
 class EyeStrainDetector:
     def __init__(self):
